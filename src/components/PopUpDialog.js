db35c69f7a1e0c21830ff09429cb45089513a4e0
Tunir Kapil <tunirkapil@gmail.com>
2017-03-18 18:31:44 -0700

    update of popupdialog to include initial state of popup and have learn more close action

--- src/components/PopUpDialog.js
+++ src/components/PopUpDialog.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, Modal} from 'react-bootstrap';
 
-class PopUpDialog extends Component{
+const PopUpDialog = React.createClass({
+  getInitialState(){
+    return {show: true};
+  },
+
+  close(){
+    this.setState({show: false});
+  },
+
+  open(){
+    this.setState({show: true});
+  },
+
   render(){
+    const close = () => this.setState({show:false});
+    //todo: link buttons on click
     const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
-    return(
-        <div className="well" style={wellStyles}>
-          <Button bsStyle="primary" bsSize="large" href="#" block>Find a Kit</Button>
-          <Button bsStyle="primary" bsSize="large" href="#" block>Report a Incident</Button>
-          <Button bsStyle="primary" bsSize="large" href="#" block>Learn More</Button>
-        </div>
-      );
-    }
-  }
+    return (
+          <Modal show = {this.state.show} onHide={close} container={this} tr bsSize="large" aria-labelledby="contained-modal-title-lg">
+            <Modal.Body>
+            <Button bsStyle="primary" bsSize="large" href="#" block>Find a Kit</Button>
+            <Button bsStyle="primary" bsSize="large" href="#" block>Report a Incident</Button>
+            <Button bsStyle="primary" bsSize="large" href="#" block onClick={() => this.setState({show: false})}>Learn More</Button>
+            </Modal.Body>
+            <Modal.Footer>In Case of an Emergency, Contact 911</Modal.Footer>
+          </Modal>
+        );
+      }
+    });
 export default PopUpDialog;
