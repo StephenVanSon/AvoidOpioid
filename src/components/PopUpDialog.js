12cd30a1038dcbac0045766e57a5920787682a6d
Tunir Kapil <tunirkapil@gmail.com>
2017-03-18 19:45:53 -0700

    updates to popup dialog to have hover over text. Minor string changes.

--- src/components/PopUpDialog.js
+++ src/components/PopUpDialog.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import {Button, Modal} from 'react-bootstrap';
+import {Button, Modal, OverlayTrigger,Tooltip,Popover} from 'react-bootstrap';
 
 const PopUpDialog = React.createClass({
   getInitialState(){
     return {show: true};
-  },
-
-  close(){
-    this.setState({show: false});
   },
 
   open(){
@@ -16,16 +12,30 @@ const PopUpDialog = React.createClass({
 
   render(){
     const close = () => this.setState({show:false});
+    const popoverReport = (
+      <Popover id="popover-trigger-hover-focus" title="Report an Incident"> If you know or think you or someone has overdosed please click this button.</Popover>
+    );
+    const popoverFind = (
+      <Popover id="popover-trigger-hover-focus" title="Find a Kit">Locate a kit near you for yourself or someone you know </Popover>
+    );
+    const popoverLearn = (
+      <Popover id="popover-trigger-hover-focus" title="Learn More">Learn more about the dangers and prevention methods of Fentanyl</Popover>
+    );
     //todo: link buttons on click
-    const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
     return (
           <Modal show = {this.state.show} onHide={close} container={this} tr bsSize="large" aria-labelledby="contained-modal-title-lg">
             <Modal.Body>
-            <Button bsStyle="primary" bsSize="large" href="#" block>Find a Kit</Button>
-            <Button bsStyle="primary" bsSize="large" href="#" block>Report a Incident</Button>
-            <Button bsStyle="primary" bsSize="large" href="#" block onClick={() => this.setState({show: false})}>Learn More</Button>
+            <OverlayTrigger placement="bottom" overlay={popoverReport}>
+              <Button bsStyle="primary" bsSize="large" href="/report" block>REPORT</Button>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={popoverFind}>
+              <Button bsStyle="primary" bsSize="large" href="/find" block>FIND</Button>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={popoverLearn}>
+              <Button bsStyle="primary" bsSize="large" href="#" block onClick={() => this.setState({show: false})}>LEARN</Button>
+            </OverlayTrigger>
             </Modal.Body>
-            <Modal.Footer>In Case of an Emergency, Contact 911</Modal.Footer>
+            <Modal.Footer>If it looks serious, it's probably serious. Don't risk a life and contact 911 now.</Modal.Footer>
           </Modal>
         );
       }
